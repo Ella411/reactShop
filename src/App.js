1fb8ab6ea5a8444a6cfd68592c5adc2b9a628a59
이승윤 <margiella.lee@gmail.com>
2022-11-30 15:29:03 +0900

    홈&상세페이지 컴포넌트 분리 전

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Container, Nav, Navbar, Row, Col} from 'react-bootstrap';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
@@ -14,31 +15,41 @@ function App() {
         <Container className='nav-container'>
           <Navbar.Brand href="#home">SHOP</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+          <Link to="/" className='link'>홈</Link>
+          <Link to="/detail" className='link'>상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
 
-      <Container>
-        <Row className='body-row'>
-          {
-            jewerly.map(function(a, i){
-              return(
-                <Modal jewerly={jewerly[i]}/>
-              )
-            })
-          }
-        </Row>
-      </Container>
+
+      <Routes>
+        <Route path='/' element={
+          <>
+          <div className='main-bg'></div>
+            <Container>
+              <Row className='body-row'>
+                {
+                  jewerly.map(function(a, i){
+                    return(
+                      <Card jewerly={jewerly[i]}/>
+                    )
+                  })
+                }
+              </Row> 
+            </Container>
+          </>
+        }/>
+
+        <Route path='/detail' element={<div>상세페이지</div>}/>
+      </Routes>
+
 
     </div>
   );
 }
 
-function Modal(props){
+function Card(props){
   return(
     <Col sm>
       <img src={process.env.PUBLIC_URL + '/' + (props.jewerly.id +1) + '.jpg'} width="80%"/>
