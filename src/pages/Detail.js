5f215824095a8efe6a07c1601156721f2dcd4a92
이승윤 <margiella.lee@gmail.com>
2022-12-02 19:16:38 +0900

    tab 전환 애니메이션

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
@@ -23,6 +24,7 @@ function Detail(props){
     return x.id == id
   });
   let [input, setInput] = useState('');
+  let [tab, setTab] = useState(0);
 
   useEffect(()=>{
     // let a = setTimeout(()=>{ setAlert(false) }, 2000 )
@@ -38,7 +40,7 @@ function Detail(props){
   }, [input])
  
     return(
-        <>
+      
         <div className='container'>
 {/* 
           {aler t_a == true ? <div className="alert alert-warning">2초 이내 구매시 할인</div> : null}
@@ -62,10 +64,48 @@ function Detail(props){
             </div>
           </div>
   
+
+          <Nav variant="tabs" defaultActiveKey="link0">
+            <Nav.Item>
+              <Nav.Link eventKey="link0" onClick={()=>{setTab(0)}}>버튼0</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link1" onClick={()=>{setTab(1)}}>버튼1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link2" onClick={()=>{setTab(2)}}>버튼2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <TabContent tab={tab}/>
+
+
+
+
+
+
         </div>
-        </>
     )
   }
-  
+
+
+                  // props 대신 {tab} 쓸 수 있음 
+  function TabContent({tab}){
+    let [fade, setFade] = useState(''); 
+
+    useEffect(()=>{
+      setTimeout(()=>{setFade('end')}, 10)
+      return()=>{
+        setFade('')
+
+      }
+
+      // tab 변경될 때마다 코드 실행해줌 
+    }, [tab])
+
+   return (<div className={`start ${fade}`}>
+    {[<div>내용0</div>,  <div>내용1</div>,  <div>내용2</div>][tab]}
+    </div>)
+  }
+
 
   export default Detail;
